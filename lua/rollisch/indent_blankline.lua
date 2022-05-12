3d9986586dcbbc69eaecef7b303672fc2400410e
Michael Rolli <michael@rollis.ch>
2022-05-12 13:05:47 +0200

    Don't decorate first indentation level

--- lua/rollisch/indent_blankline.lua
+++ lua/rollisch/indent_blankline.lua
@@ -42,7 +42,7 @@ M.config = function()
       "IndentBlanklineIndent2",
     },
     show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
+    show_first_indent_level = false,
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
