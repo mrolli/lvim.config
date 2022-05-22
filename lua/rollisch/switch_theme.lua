25b93932145c4732826a529e8df9791be575a0c8
Michael Rolli <michael@rollis.ch>
2022-05-22 10:21:02 +0200

    Add config settings for gruvbox including defaults

--- lua/rollisch/switch_theme.lua
+++ lua/rollisch/switch_theme.lua
@@ -3,10 +3,18 @@
 local M = {}
 
 M.to_gruvbox_material = function ()
+  -- See :h gruvbox-material for docs
+  vim.g.gruvbox_material_palette = 'material'
   vim.g.gruvbox_material_background = 'soft'
   vim.g.gruvbox_material_enable_bold = 1
   vim.g.gruvbox_material_enable_itablic = 1
+  -- vim.g.gruvbox_material_visual = 'blue background'
+  vim.g.gruvbox_material_spell_foreground = 'colored'
   vim.g.gruvbox_material_ui_contrast = 'low'
+  vim.g.gruvbox_material_diagnostic_text_highlight = 1
+  vim.g.gruvbox_material_diagnostic_line_highlight = 1
+  vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+  vim.g.gruvbox_material_better_performance = 1
 
   lvim.colorscheme = "gruvbox-material"
   lvim.builtin.lualine.theme = "gruvbox-material"
