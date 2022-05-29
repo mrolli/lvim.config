54282df137d725153ba282865c0fdb074201f68f
Michael Rolli <michael@rollis.ch>
2022-05-29 10:46:44 +0200

    Adjusted autocommands due to changed API

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,14 +1,28 @@
 --[[
-Change/add autocommands
+Autocommands (https://neovim.io/doc/user/autocmd.html)
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.json", "*.jsonc" },
+  -- enable wrap mode for json files only
+  command = "setlocal wrap",
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "zsh",
+  callback = function()
+    -- let treesitter use bash highlight for zsh files as well
+    require("nvim-treesitter.highlight").attach(0, "bash")
+  end,
+})
+
 ]]
 local M = {}
 
 M.config = function()
-  lvim.autocommands.custom_groups = {
-    --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
     -- do not automatically strip whitespace from markdown files
-    { "FileType", "markdown", ":DisableStripWhitespaceOnSave" },
-  }
+    command = ":DisableStripWhitespaceOnSave",
+  })
 
 end
 
