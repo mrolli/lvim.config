5eaad3fd1c0b9baa0c87a63b0049a64c27b07d05
Michael Rolli <michael@rollis.ch>
2022-11-11 10:50:37 +0100

    Remove dead config

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,6 +1,2 @@
 -- set textwidth to automatically hard wrap lines
 vim.opt.textwidth = 80
-
--- register markdownlint as diagnostic source
--- local null_ls = require("null-ls")
--- null_ls.register(markdownlint)
